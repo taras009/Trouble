25cd8bc36da7c758a741b731da6d3e0e9277730d
Anadi Misra <amisra@luc.edu>
2018-04-22 11:55:47 -0500

    created new test classes and test methods

--- test_src/com/UI/TroubleBoardTest.java
+++ test_src/com/UI/TroubleBoardTest.java
@@ -1,7 +1,30 @@
 package com.UI;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class TroubleBoardTest  {
+public class TroubleBoardTest {
 
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void createBoard() {
+    }
+
+    @Test
+    public void buttonControl() {
+    }
+
+    @Test
+    public void isWinner() {
+    }
+
+    @Test
+    public void movesAvailable() {
+        /* int testCount = 0;
+        assertEquals(true, (testCount < 5)); */
+    }
 }
