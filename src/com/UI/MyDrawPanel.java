3e6b6fe920e36c9caada21ac93eb2bc2bde0ab99
Dariusz Jedralski <djedralski@gmail.com>
2018-05-01 23:22:03 -0500

    removed static from move so game would work

--- src/com/UI/MyDrawPanel.java
+++ src/com/UI/MyDrawPanel.java
@@ -316,7 +316,7 @@ public class MyDrawPanel extends JPanel {
     }
 
 
-    public static void move(int diceNumber, int pieceId) {
+    public void move(int diceNumber, int pieceId) {
         System.out.println("PieceID:" + pieceId);
         switch (chanceToPlay) {
             case RED:
